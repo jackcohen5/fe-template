e2ef6e560b4f50c573e63a6c0f8ef8d65049417c
Jack Cohen <thejackcohen@gmail.com>
2020-12-22 22:20:13 -0500

    fix origin

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -15,24 +15,32 @@ if (process.env.NODE_ENV !== 'development') {
 
 const store = configureStore()
 
-const BootstrappedApp = ({ Component, pageProps }) => (
-    <div style={{ fontFamily: FontFamily }}>
-        <ErrorBoundary tag="root">
-            <Auth0Provider
-                audience={AUTH0_AUDIENCE}
-                domain={AUTH0_DOMAIN}
-                clientId={AUTH0_CLIENT_ID}
-                useRefreshTokens={true}
-                redirectUri={`http://localhost:3000${routes.PROTECTED_HOME}`}
-                // redirectUri={`${window.location.origin}${routes.PROTECTED_HOME}`}
-            >
-                <Provider store={store}>
-                    <Component {...pageProps} />
-                </Provider>
-            </Auth0Provider>
-        </ErrorBoundary>
-    </div>
-)
+const BootstrappedApp = ({ Component, pageProps }) => {
+    let origin
+    try {
+        origin = window?.location?.origin
+    } catch (e) {
+        origin = 'http://localhost:3000'
+    }
+
+    return (
+        <div style={{ fontFamily: FontFamily }}>
+            <ErrorBoundary tag="root">
+                <Auth0Provider
+                    audience={AUTH0_AUDIENCE}
+                    domain={AUTH0_DOMAIN}
+                    clientId={AUTH0_CLIENT_ID}
+                    useRefreshTokens={true}
+                    redirectUri={`${origin}${routes.PROTECTED_HOME}`}
+                >
+                    <Provider store={store}>
+                        <Component {...pageProps} />
+                    </Provider>
+                </Auth0Provider>
+            </ErrorBoundary>
+        </div>
+    )
+}
 
 BootstrappedApp.propTypes = {
     Component: PropTypes.any.isRequired,

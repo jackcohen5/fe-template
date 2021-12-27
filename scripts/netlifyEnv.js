39480345c9c1434f252f61f1bffb1559a653b7f6
Jack Cohen <thejackcohen@gmail.com>
2021-12-27 14:20:02 -0500

    netlify api

--- scripts/netlifyEnv.js
+++ scripts/netlifyEnv.js
@@ -5,7 +5,10 @@ fs.readFile('netlify.toml', 'utf8', function (err, data) {
     if (err) {
         return console.error(err)
     }
-    const result = data.replace(/API_REDIRECT/g, process.env.API_LOCATION)
+    const result = data.replace(
+        /API_REDIRECT/g,
+        `${process.env.API_LOCATION}/api/:splat`,
+    )
 
     fs.writeFile('netlify.toml', result, 'utf8', function (err) {
         if (err) return console.error(err)
